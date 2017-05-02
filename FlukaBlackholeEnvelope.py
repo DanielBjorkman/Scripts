2a8dae1903be172be05810c650f40498cf58d308
DanielBjorkman <daniel.bjorkman@cern.ch>
2017-05-02 16:07:09 +0200

    Working copy

--- FlukaBlackholeEnvelope.py
+++ FlukaBlackholeEnvelope.py
@@ -1,16 +1,25 @@
 
 
 
-directory = '//cern.ch/dfs/Users/c/cbjorkma/Documents'
-filename = 'Problem2Bias.new.inp'
+#directory = '//cern.ch/dfs/Users/c/cbjorkma/Documents'
+#filename = 'DumpNewCoreResidualRef.inp'
 
-Envfilename = 'Env' + filename
+directory = '//rpclustersrv1/cbjorkma/Dump studies'
+filename = 'DumpResMarbleBack15cm.inp'
+
+
+
+
+Inverse = 1
+
+if Inverse == 0:
+    Envfilename = 'Env' + filename
+else:
+    Envfilename = 'EnvInv' + filename
+
 
 import os
 os.chdir(directory)
-
-#from shutil import copyfile
-#copyfile(filename, Envfilename)
 
 info = {'Geobegin':[],'end1':[],'end2':[],'newline':[],'position':[], 'BLCKHOLE':[]}
 
@@ -30,99 +39,60 @@ with open(filename) as file:
             info['BLCKHOLE'].append(line_content[2])
          row = row +1
 
-#print(info['Geobegin'][0])
-#print(info['end1'][0])
-#print(info['end2'][0])
 
 String = '+Envelope '
 negString = '-Envelope '
 
-EnvelopeBody = 'RPP Envelope   -100.00 100.00 -100.00 100.00 -100.00 100.00\n'
+EnvelopeBody = 'RPP Envelope   -1000.00 1000.00 -1000.00 1000.00 -1000.00 1000.00\n'
 
 f = open(filename,"r+")
 lines = f.readlines()
 
+#Modify all regions
 for row in range(0,len(lines)):
-    #Add Envelope body
- #   if row == info['Geobegin'][0]+2:
- #       lines.append(EnvelopeBody)
-    #Modify all regions
     if row > info['end1'][0] +1 and row < info['end2'][0] :
+        if row == 446:
+            a = 0
         line_content = lines[row].split(' ')
+        newExpression = 0
+        if len(lines[row]) >= 15:
+            if lines[row][15] == '|' or lines[row][13] == '5':
+                newExpression = 1
+        changed = 0
         if line_content[0] != '*':
-            if line_content[0] != info['BLCKHOLE'][0]:
-                info['newline'].append(lines[row][:15] + String + lines[row][15:])
-                info['position'].append(row)
-                lines[row]=lines[row][:15] + String + lines[row][15:]
-            else:
-                info['newline'].append(lines[row][:15] + negString + lines[row][15:])
-                info['position'].append(row)
+
+            #Scenario 1: Region containing the black hole
+            if line_content[0] == info['BLCKHOLE'][0]:
                 lines[row] = lines[row][:15] + negString + '\n'
+                changed = 1
+
+            #Scenario 2: Multiple zones on the same line
+            idx = []
+            for i in range(0,len(lines[row])):
+                if int(lines[row][i] == '|'):
+                    idx.append(i)  
+            if len(idx) > 1 and not changed:
+                lines[row]=lines[row][:15] + String + lines[row][15:]
+                for i in range(0,len(idx)):
+                    lines[row]=lines[row][:(idx[i] +2 +(i+1)*10)] + String + lines[row][(idx[i] +2 +(i+1)*10):]
+                changed = 1
+
+            #Scenario 3: For regions of a single zone, or multiple zones on different lines
+            if len(lines[row]) >= 15 and not changed and newExpression:
+                if lines[row][15] == '|': 
+                     lines[row]=lines[row][:17] + String + lines[row][17:]
+                else:
+                     lines[row]=lines[row][:15] + String + lines[row][15:]
 
 
+#Add Envelope body
 lines2 = lines[0:info['Geobegin'][0]+2]
 lines2.append(EnvelopeBody)
 lines2[info['Geobegin'][0]+3:] = lines[info['Geobegin'][0]+2:]
-#lines2.append(lines[info['Geobegin'][0]+2:])
 
-#append(EnvelopeBody,lines,info['Geobegin'][0]+2)
-#lines[info['Geobegin'][0]+3:] = lines[info['Geobegin'][0]+2:]
-#lines.insert(info['Geobegin'][0]+2, EnvelopeBody)
-
-#for i in range(0,len(list)):
- #   if i
-
+#Write
 with open(Envfilename, 'w') as output:
     output.writelines(lines2)
 
 
-a = 0
 
-
-#lines.insert(info['Geobegin'][0]+2,EnvelopeBody)
-#f.close()
-
-#with open(Envfilename, 'r+') as output:
- #   output.seek(40,0)
-  #  output.write(EnvelopeBody)
-
-    #info['Geobegin'][0]+2
-
-#with open(Envfilename) as file, open(Envfilename, 'w') as output:
- #   output.insert(EnvelopeBody)
-    
-    #for line in file.readlines():
-     #   if row == info['Geobegin'][0]+2:
-      #      output_file.write(EnvelopeBody)         
-       # else:
-        #    output_file.write(line)
-
-   
-   # for line in file.readlines():
-    #     if row == info['Geobegin'][0]+2:
-     #            output_file.write(EnvelopeBody)
-      #   row = row + 1
-
-#f = open("Envfilename", "r")
-#contents = f.readlines()
-#f.close()
-
-#contents.insert(info['Geobegin'][0]+2, EnvelopeBody)
-
-#f = open("path_to_file", "w")
-#contents = "".join(contents)
-#f.write(contents)
-#f.close()
-
-
-#for r in range(0,len(info['position'])):
- #   row = 0
-  #  if row == 66:
-   #     a = 0
-    #with open(filename, 'r') as input_file, open(Envfilename, 'w') as output_file:
-     #   for line in input_file:
-      #      if info['position'][r] == row:
-       #         output_file.write(info['newline'][r] + '\n')
-        #    else:
-         #       output_file.write(line)
-        #row = row +1 
